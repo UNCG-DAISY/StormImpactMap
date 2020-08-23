2f317060a77b4a491014a9d4896ad6221097de23
Jamison Valentine <jamisont.valentine@gmail.com>
2020-08-23 11:49:43 -0400

    add mapview to web page using leaflet js

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-def testFunction():
-    return "this is my first flaks application"
-
 @app.route("/")
+def testFunction():
+    return render_template("index.html")
+
+@app.route("/about")
+def renderAbout():
+    return"<h1>Here's the About Page</h1>"
+
+
+if __name__ == '__main__':
+    # will auto restart server upon save
+    app.run(debug=True)
