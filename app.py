d32553e327c5407fe1098edb19e7115930d787ff
Jamison Valentine <jamisont.valentine@gmail.com>
2020-08-24 20:07:49 -0400

    add new test endpoing (/data)

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import fetch_data
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +10,10 @@ def testFunction():
 @app.route("/about")
 def renderAbout():
     return"<h1>Here's the About Page</h1>"
+
+@app.route("/data")
+def renderImgData():
+    return fetch_data.get_img_data()
 
 
 if __name__ == '__main__':
