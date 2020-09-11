02b87f82dfa9aca58fa0bfcf8b401efb61a4616e
Jamison Valentine <jamisont.valentine@gmail.com>
2020-09-11 15:18:12 -0400

    move shapefile integration methods to js/shapefiles.js

---
+++ js/shapefiles.js
@@ -0,0 +1,35 @@
+function addShapefileLayers() {
+  /*
+  ///////////////////////////////////////////////////////// SHAPEFILES
+  */
+
+  assignColor = function (feature) {
+    let povw = feature.properties.POVW;
+    if (povw >= 80) {
+      return { color: "#ff0000" };
+    } else if (povw < 80 && povw >= 60) {
+      return { color: "#ff3700" };
+    } else if (povw < 60 && povw >= 40) {
+      return { color: "#ff5703" };
+    } else if (povw >= 20) {
+      return { color: "#ffb477" };
+    } else {
+      return { color: "#ffd840" };
+    }
+  };
+  const florence_pred = new L.Shapefile("data/shapefiles/Florence_2018.zip", {
+    style: assignColor,
+  });
+
+  const michael_pred = new L.Shapefile("data/shapefiles/Michael_2018.zip", {
+    style: assignColor,
+  });
+
+  const michael_wash = new L.Shapefile(
+    "data/shapefiles/Michael_Overwash_Extent.zip"
+  );
+
+  const florence_wash = new L.Shapefile(
+    "data/shapefiles/Florence_Overwash_Extent.zip"
+  );
+}
--- js/storms.js
+++ js/storms.js
@@ -1,7 +1,7 @@
 function addStormLayers() {
   /*
-        //////////////////// ISAIAS ///////////////////////////////
-    */
+  //////////////////// ISAIAS ///////////////////////////////
+  */
 
   const aug03a = new L.mapbox.tileLayer(
     "https://storms.ngs.noaa.gov/storms/tileso/services/tileserver.php?/20200803a-rgb.json",
@@ -17,8 +17,8 @@ function addStormLayers() {
   );
 
   /*
-    //////////////////// DORIAN ///////////////////////////////
-*/
+  //////////////////// DORIAN ///////////////////////////////
+  */
 
   var sep04a = new L.mapbox.tileLayer(
     "https://storms.ngs.noaa.gov/storms/tilesm/services/tileserver.php?/20190904a-rgb.json",
@@ -72,94 +72,129 @@ function addStormLayers() {
     "https://storms.ngs.noaa.gov/storms/tilesn/services/tileserver.php?/20190920a-rgb.json",
     { errorTileUrl: "../dorian/images/clear.png" }
   );
-}
 
-/*
+  /*
     //////////////////// MICHAEL ///////////////////////////////
 */
 
-const oct11a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181011a-rgb.json",
-  { errorTileUrl: "../michael/images/clear.png" }
-);
-const oct12a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181012a-rgb.json",
-  { errorTileUrl: "../michael/images/clear.png" }
-);
-const oct12b = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181012b-rgb.json",
-  { errorTileUrl: "../michael/images/clear.png" }
-);
-const oct13a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181013a-rgb.json",
-  { errorTileUrl: "../michael/images/clear.png" }
-);
-const oct14a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181014a-rgb.json",
-  { errorTileUrl: "../michael/images/clear.png" }
-);
+  const oct11a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181011a-rgb.json",
+    { errorTileUrl: "../michael/images/clear.png" }
+  );
+  const oct12a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181012a-rgb.json",
+    { errorTileUrl: "../michael/images/clear.png" }
+  );
+  const oct12b = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181012b-rgb.json",
+    { errorTileUrl: "../michael/images/clear.png" }
+  );
+  const oct13a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181013a-rgb.json",
+    { errorTileUrl: "../michael/images/clear.png" }
+  );
+  const oct14a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesj/services/tileserver.php?/20181014a-rgb.json",
+    { errorTileUrl: "../michael/images/clear.png" }
+  );
 
-/*
+  /*
 //////////////////// FLORENCE ///////////////////////////////
 */
 
-const sept15a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180915a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept16a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180916a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept16b = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180916b-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept17a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180917a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept18a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180918a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept19a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180919a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept19b = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180919b-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept19c = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180919c-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept20a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180920a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept20b = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180920b-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept20c = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180920c-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept21a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180921a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept21b = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180921b-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept22a = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180922a-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
-const sept22b = new L.mapbox.tileLayer(
-  "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180922b-rgb.json",
-  { errorTileUrl: "../florence/images/clear.png" }
-);
+  const sept15a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180915a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept16a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180916a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept16b = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180916b-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept17a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180917a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept18a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180918a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept19a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180919a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept19b = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180919b-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept19c = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180919c-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept20a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180920a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept20b = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180920b-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept20c = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180920c-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept21a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180921a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept21b = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180921b-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept22a = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180922a-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+  const sept22b = new L.mapbox.tileLayer(
+    "https://storms.ngs.noaa.gov/storms/tilesi/services/tileserver.php?/20180922b-rgb.json",
+    { errorTileUrl: "../florence/images/clear.png" }
+  );
+
+  const isaias = L.layerGroup([aug03a, aug04a, aug05a]);
+  const florence = L.layerGroup([
+    sept15a,
+    sept16a,
+    sept16b,
+    sept17a,
+    sept18a,
+    sept19a,
+    sept19b,
+    sept19c,
+    sept20a,
+    sept20b,
+    sept20c,
+    sept21a,
+    sept21b,
+    sept22a,
+    sept22b,
+  ]);
+  const michael = L.layerGroup([oct11a, oct12a, oct12b, oct13a, oct14a]);
+  const dorian = L.layerGroup([
+    sep04a,
+    sep05a,
+    sep05b,
+    sep06a,
+    sep06b,
+    sep07a,
+    sep07b,
+    sep17a,
+    sep17b,
+    sep18a,
+    sep18b,
+    sep19a,
+    sep20a,
+  ]);
+}
