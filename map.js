a73f08fe26c43ff416f33a568b340d7bccb64ab4
Jamison Valentine <jamisont.valentine@gmail.com>
2020-09-11 15:19:56 -0400

    move icon methods to js/marker-icons.js

--- map.js
+++ map.js
@@ -52,34 +52,6 @@ const overlayLayers = {
   "Florence Overwash": florence_wash,
 };
 
-/*
-        //////////////////////////////////////////////////////////////////////////////////////
-            ADDING MARKERS TO THE MAP
-        //////////////////////////////////////////////////////////////////////////////////////
-        */
-
-const greenIcon = new L.Icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
-const redIcon = new L.Icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
 getSampleData("data/HurricaneIsaiasSampleData.csv", 1);
 
 function getSampleData(url, order) {
