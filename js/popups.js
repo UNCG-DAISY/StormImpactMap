80b7a60ee6176e4a5ae05f76ba01d937315bd852
Evan B. Goldstein <ebgoldstein@gmail.com>
2020-09-28 12:15:29 -0400

    add second link

--- js/popups.js
+++ js/popups.js
@@ -1,5 +1,6 @@
 img_base_url = "https://coastalimagelabeler.science/api/image/";
-img_type = "/gradcam";
+img_type = "/original";
+img_grad = "/gradcam";
 
 function getSampleData(url, order) {
   let csv_data = "";
@@ -40,6 +41,12 @@ function getSampleData(url, order) {
 
         let popupLink = document.createElement("a");
         popupLink.href = img_base_url + id + img_type;
+        popupLink.style.display = "block";
+        popupLink.target = "_blank";
+        popupLink.text = "View Image";
+
+        let popupLink = document.createElement("a");
+        popupLink.href = img_base_url + id + img_grad;
         popupLink.style.display = "block";
         popupLink.target = "_blank";
         popupLink.text = "View ML results";
