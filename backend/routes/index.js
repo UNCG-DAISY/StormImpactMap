553456bc6b96d631b5c46cb29c1aef5f9a08d4cf
Jamison Valentine <jamisont.valentine@gmail.com>
2020-12-21 15:45:25 -0500

    complete express backend initial setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('done');
-  res.sendFile("../../index.html");
 });
 
 module.exports = router;
--- backend/routes/map.js
+++ backend/routes/map.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send("why not working");
+  
+  res.sendFile('index.html', {root: path.join(__dirname, '../views/')}, function (err) {
+    if (err) {
+      console.log("there was an error");
+  } else {
+    console.log("successful");
+  }});
 });
 
 module.exports = router;
