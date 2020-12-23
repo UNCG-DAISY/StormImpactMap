dfb3849c2f59a4ddc65a750e8669f3e3c7617331
Jamison Valentine <jamisont.valentine@gmail.com>
2020-12-23 13:02:00 -0500

    add skeleton functions and config file for storm dir

--- admin.js
+++ admin.js
@@ -1,8 +1,36 @@
 fs = require('fs');
-prompt = requirte('prompt')
+// prompt = require('prompt')
 
+/*
 
+NOAA NGS Emergency Response Imagery tiles
+USGS overwash predictions
+USGS overwash observations
+NOAA NHC Hurricane Tracks
+ML observations of washover
 
-fs.readdir('src/public/data/storms', (err, files) => {
+*/
+
+const storm_dir_path = 'src/public/data/storms';
+
+fs.readdir(storm_dir_path, (err, files) => {
     console.log(files);
 });
+
+function add_overwash_extents() {
+
+}
+
+function add_overwash_predictions() {
+    
+}
+
+function add_hurricane_tracks() {
+    
+}
+
+function add_ml_observations() {
+    
+}
+
+
